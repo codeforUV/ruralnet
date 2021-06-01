b912ab098f3cb3f72984a0dc3b5e89bc826f3ffe
NateDimick <natdimick@gmail.com>
2021-06-01 18:08:27 -0400

    add btn color classes

--- src/routes/speed.svelte
+++ src/routes/speed.svelte
@@ -16,8 +16,8 @@
 
 
 <h1 id='title'>New Speed Test</h1>
-<button id='test' on:click={() => {st.startTest()}}>Click to begin Speedtest</button>
-<button disabled='true' id='cancel' on:click={() => {st.abortTest()}}>Cancel Test</button>
+<button id='test' class="btn btn-blue" on:click={() => {st.startTest()}}>Click to begin Speedtest</button>
+<button disabled='true' id='cancel' class="btn btn-red" on:click={() => {st.abortTest()}}>Cancel Test</button>
 <p id='result'></p>
 <p id='done'></p>
 
