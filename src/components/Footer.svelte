0a319197045261c3996aca182f655f9cc14ffd7b
Colby Hemond <hemond.colby@gmail.com>
2021-08-02 19:05:09 -0400

    fix diff

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,46 +2,6 @@
 </script>
 
 <footer id="footer">
-<<<<<<< HEAD
-    <div class="container footer-container">
-      <div class="row">
-        <div class="col-md-3">
-          <!-- <img class="logo" src="images/logo.png" alt=""> -->
-          <!-- <p class="title">Social</p> -->
-          <!-- <a href="#" class="app_download"><img src="images/app-store-download.png" alt=""></a> -->
-          <ul class="socials">
-            <li><a href="https://twitter.com"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://facebook.com"><i class="fa fa-facebook"></i></a></li>
-          </ul>
-        </div>
-        <div class="col-md-2 col-md-offset-3 col-sm-4 col-xs-6 footer-links">
-          <ul>
-            <li><p class="title">Tools</p></li>
-            <!-- <li><a href="/">How it works?</a></li>
-            <li><a href="/">Our Mission</a></li>
-            <li><a href="/">Recent News</a></li> -->
-            <li><a href="/speed">Speed Test</a></li>
-            <li><a href="/survey">Survey</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 footer-links">
-          <ul>
-            <li><p class="title">About Us</p></li>
-            <li><a href="about">Rural Net</a></li>
-            <li><a href="https://codeforuv.org/">Code for Upper Valley</a></li>
-            <li><a href="https://www.codeforamerica.org/">Code for America</a></li>
-            <li><a href="about/privacy">Privacy policy</a></li>
-            <li><a href="/">Terms & Conditions</a></li>
-          </ul>
-        </div>
-        <div class="col-md-2 col-sm-4 col-xs-6 footer-links">
-          <ul>
-            <li><p class="title">Support</p></li>
-            <li><a href="/">F.A.Q.</a></li>
-            <li><a href="/">Contact us</a></li>
-          </ul>
-        </div>
-=======
   <div class="container footer-container">
     <div class="row">
       <div class="col-md-3">
@@ -77,7 +37,6 @@
           <li><a href="/">F.A.Q.</a></li>
           <li><a href="/">Contact us</a></li>
         </ul>
->>>>>>> 6501e4ed1c0adc708ada64c402aed25d47fbc712
       </div>
     </div>
   </div>
